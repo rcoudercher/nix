ed00e845b1e925121b2bd562f29ad59dc2740742
rcoudercher <coudercher.remy@gmail.com>
2024-11-17 10:51:23 +0100

    autohide dock

--- flake.nix
+++ flake.nix
@@ -80,6 +80,10 @@
   		done
 		'';
 
+	system.defaults = {
+		dock.autohide = true;
+	};
+
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
