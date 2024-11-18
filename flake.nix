392615b5c8a309e8e9353f536d8e74afd975d1a4
rcoudercher <coudercher.remy@gmail.com>
2024-11-18 07:10:03 +0100

    Uninstalling nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  outputs = inputs@{
+	self,
+	nix-darwin,
+	nixpkgs,
+	# nix-homebrew
+}:
   let
     configuration = { pkgs, config, ... }: {
 
@@ -16,10 +21,10 @@
 
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-	pkgs.alacritty
+      environment.systemPackages = with pkgs;  [
+	# pkgs.alacritty
 	pkgs.bruno
-	pkgs.discord
+	# pkgs.discord
 	pkgs.fx
 	pkgs.k9s
 	pkgs.kubectx
@@ -30,7 +35,7 @@
 	pkgs.mkcert
 	pkgs.neovim
 	pkgs.ngrok
-	pkgs.obsidian
+	# pkgs.obsidian
 	pkgs.postman
 	pkgs.php83
 	pkgs.slack
@@ -38,30 +43,37 @@
 	pkgs.tea
 	pkgs.tmux
 	# pkgs.tor
-	pkgs.vscode
+	(pkgs.vscode-with-extensions.override {
+		vscodeExtensions = with pkgs.vscode-extensions; [
+			bmewburn.vscode-intelephense-client
+			esbenp.prettier-vscode
+			equinusocio.vsc-material-theme-icons
+			vue.volar
+		];
+	})
 	];
 
-	homebrew = {
-		enable = true;
-		brews = [
-			# Find app ids on the mac app store
-			"mas"
-			"nvm"
-		];
-		casks = [
-			"firefox"
-			"vlc"
-		];
-
-		masApps = {
-			"Trello" = 1278508951;
-		};
-
-		# Ensure removal of casks not in the list above
-		onActivation.cleanup = "zap";
-		onActivation.autoUpdate = true;
-		onActivation.upgrade = true;
-	};
+	# homebrew = {
+	#	enable = true;
+	#	brews = [
+	#		# Find app ids on the mac app store
+	#		"mas"
+	#		"nvm"
+	#	];
+	#	casks = [
+	#		"firefox"
+	#		"vlc"
+	#	];
+	#
+	#	masApps = {
+	#		"Trello" = 1278508951;
+	#	};
+	#
+	#	# Ensure removal of casks not in the list above
+	#	onActivation.cleanup = "zap";
+	#	onActivation.autoUpdate = true;
+	#	onActivation.upgrade = true;
+	#};
 	
 	fonts.packages = [
 		(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
@@ -89,17 +101,20 @@
 
 	system.defaults = {
 		dock.autohide = true;
+
 		dock.persistent-apps = [
-		"${pkgs.alacritty}/Applications/Alacritty.app"
-		"/Applications/Firefox.app"
-		"${pkgs.discord}/Applications/Discord.app"
-		"${pkgs.obsidian}/Applications/Obsidian.app"
-		"${pkgs.vscode}/Applications/Visual Studio Code.app"
-		"/System/Applications/Mail.app"
-		"/System/Applications/Calendar.app"
+			# "${pkgs.alacritty}/Applications/Alacritty.app"
+			"/Applications/Firefox.app"
+			"${pkgs.discord}/Applications/Discord.app"
+			# "${pkgs.obsidian}/Applications/Obsidian.app"
+			"${pkgs.vscode}/Applications/Visual Studio Code.app"
+			"/System/Applications/Mail.app"
+			"/System/Applications/Calendar.app"
 		];
+
 		# https://mynixos.com/nix-darwin/option/system.defaults.finder.FXPreferredViewStyle
 		finder.FXPreferredViewStyle = "clmv";
+		
 		NSGlobalDomain.AppleICUForce24HourTime = true;
 	};
 
@@ -126,22 +141,22 @@
     darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
       modules = [
 	configuration
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-		nix-homebrew = {
-			# Install Homebrew under the default prefix
-			enable = true;
-
-			# Apple Silicon Only
-			enableRosetta = true;
-
-			# User owning the Homebrew prefix
-			user = "remy";
-			
-			# Automatically migrate existing Homebrew installations
-			autoMigrate = true;
-		};
-	}
+	# nix-homebrew.darwinModules.nix-homebrew
+	#{
+	#	nix-homebrew = {
+	#		# Install Homebrew under the default prefix
+	#		enable = true;
+	#
+	#		# Apple Silicon Only
+	#		enableRosetta = true;
+	#
+	#		# User owning the Homebrew prefix
+	#		user = "remy";
+	#		
+	#		# Automatically migrate existing Homebrew installations
+	#		autoMigrate = true;
+	#	};
+	#}
 	];
     };
 
